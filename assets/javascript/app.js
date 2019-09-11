40f3f634497e55838a5bf0757213eeae7c7e7a48
South Matic <Southmatic729@gmail.com>
2019-09-10 20:03:44 -0400

    added end stage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,228 +1,236 @@
 $(document).ready(function() {
-  // $("*").hide();
- 
+  // ============================================================================
+  // Global Variables
+  // ============================================================================
   var question1 = {
-   quest: "What's the capital of Illinois?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Chicago",
-   correct: "answer4"
+    quest: "What's the capital of Illinois?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Chicago",
+    correct: "answer4"
   };
- 
+
   var question2 = {
-   quest: "What's the capital of North Carolina?",
-   answer1: "Raleigh",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer1"
+    quest: "What's the capital of North Carolina?",
+    answer1: "Raleigh",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer1"
   };
- 
+
   var question3 = {
-   quest: "What's the capital of New York?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer2"
+    quest: "What's the capital of New York?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer2"
   };
- 
+
   var question4 = {
-   quest: "What's the capital of Pennsylvania?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Philadelphia",
-   correct: "answer4"
+    quest: "What's the capital of Pennsylvania?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Philadelphia",
+    correct: "answer4"
   };
- 
+
   var question5 = {
-   quest: "What's the capital of Colorado?",
-   answer1: "Denver",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer1"
+    quest: "What's the capital of Colorado?",
+    answer1: "Denver",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer1"
   };
- 
+
   var question6 = {
-   quest: "What's the capital of Oregon?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Portland",
-   answer4: "Houston",
-   correct: "answer3"
+    quest: "What's the capital of Oregon?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Portland",
+    answer4: "Houston",
+    correct: "answer3"
   };
   var question7 = {
-   quest: "What's the capital of Illinois?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Chicago",
-   correct: "answer4"
+    quest: "What's the capital of Illinois?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Chicago",
+    correct: "answer4"
   };
- 
+
   var question8 = {
-   quest: "What's the capital of North Carolina?",
-   answer1: "Raleigh",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer1"
+    quest: "What's the capital of North Carolina?",
+    answer1: "Raleigh",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer1"
   };
- 
+
   var question9 = {
-   quest: "What's the capital of New York?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer2"
+    quest: "What's the capital of New York?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer2"
   };
- 
+
   var question10 = {
-   quest: "What's the capital of Pennsylvania?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Philadelphia",
-   correct: "answer4"
+    quest: "What's the capital of Pennsylvania?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Philadelphia",
+    correct: "answer4"
   };
- 
+
   var question11 = {
-   quest: "What's the capital of Colorado?",
-   answer1: "Denver",
-   answer2: "New York",
-   answer3: "Miami",
-   answer4: "Houston",
-   correct: "answer1"
+    quest: "What's the capital of Colorado?",
+    answer1: "Denver",
+    answer2: "New York",
+    answer3: "Miami",
+    answer4: "Houston",
+    correct: "answer1"
   };
- 
+
   var question12 = {
-   quest: "What's the capital of Oregon?",
-   answer1: "Durham",
-   answer2: "New York",
-   answer3: "Portland",
-   answer4: "Houston",
-   correct: "answer3"
+    quest: "What's the capital of Oregon?",
+    answer1: "Durham",
+    answer2: "New York",
+    answer3: "Portland",
+    answer4: "Houston",
+    correct: "answer3"
   };
- 
+
   triviaArray = [
-   question1,
-   question2,
-   question3,
-   question4,
-   question5,
-   question6,
-   question7,
-   question8,
-   question9,
-   question10,
-   question11,
-   question12
+    question1,
+    question2,
+    question3,
+    question4,
+    question5,
+    question6,
+    question7,
+    question8,
+    question9,
+    question10,
+    question11,
+    question12
   ];
+  var right = 0;
+  var wrong = 0;
   var currentArray = [];
   var intervalId;
+  var intervalId1;
   var picked = false;
   var countNum = 0;
-  var countmax = triviaArray.length; 
   // prevents the clock from being sped up unnecessarily
   var clockRunning = false;
   var time = 30;
- 
+
   //audio variables
-  var wrongSound = new Audio("./assets/audio/Losing Horn.ogg");
-  var rightSound = new Audio("./assets/audio/Exclamation Point.ogg");
-  var timeSound = new Audio("./assets/audio/jeopardy.mp3");
- 
+  var wrongSound = new Audio("../assets/audio/Losing Horn.ogg");
+  var rightSound = new Audio("../assets/audio/winner-bell.mp3");
+  var timeSound = new Audio("../assets/audio/jeopardy.mp3");
+  var startSound = new Audio("../assets/audio/dun_dun_1.mp3");
+  var clockSound = new Audio("../assets/audio/tick.mp3");
+  clockSound.loop = true;
+  timeSound.loop = true;
   function animateCSS(element, animationName, callback) {
-   const node = document.querySelector(element);
-   node.classList.add("animated", animationName);
- 
-   function handleAnimationEnd() {
-    node.classList.remove("animated", animationName);
-    node.removeEventListener("animationend", handleAnimationEnd);
- 
-    if (typeof callback === "function") callback();
-   }
- 
-   node.addEventListener("animationend", handleAnimationEnd);
+    const node = document.querySelector(element);
+    node.classList.add("animated", animationName);
+
+    function handleAnimationEnd() {
+      node.classList.remove("animated", animationName);
+      node.removeEventListener("animationend", handleAnimationEnd);
+
+      if (typeof callback === "function") callback();
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd);
   }
- 
+
   // =============================================================================
   // console reporting function
   // =============================================================================
   function report(section) {
-   var divider = "------------------------------------";
-   console.log(" ");
-   console.log(divider);
-   console.log("running ", section);
-   console.log(divider);
+    var divider = "------------------------------------";
+    console.log(" ");
+    console.log(divider);
+    console.log("running ", section);
+    console.log(divider);
   }
- 
+
   function start() {
-   // DONE: Use setInterval to start the count here and set the clock to running.
-   if (!clockRunning) {
-    intervalId = setInterval(count, 1000);
-    clockRunning = true;
-    timeSound.play();
-   }
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+      intervalId = setInterval(count, 1000);
+      clockRunning = true;
+      clockSound.play();
+    }
   }
- 
+
   function stop() {
-   // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-   clearInterval(intervalId);
-   clockRunning = false;
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
+    clockRunning = false;
+    clockSound.pause();
   }
- 
+
   function count() {
-   // DONE: increment time by 1, remember we cant use "this" here.
-   time--;
- 
-   // DONE: Get the current time, pass that into the timeConverter function,
-   //       and save the result in a variable.
-   var converted = timeConverter(time);
-   console.log(converted);
- 
-   // DONE: Use the variable we just created to show the converted time in the "display" div.
-   $("#display").text("Time remaining: " + converted + " seconds");
+    time--;
+    var converted = timeConverter(time);
+    console.log(converted);
+    $("#display").text("Time remaining: " + converted + " seconds");
+    if (converted <= 0) {
+      stop();
+    }
   }
- 
+
   function timeConverter(t) {
-   var minutes = Math.floor(t / 60);
-   var seconds = t - minutes * 60;
- 
-   if (seconds < 10) {
-    seconds = "0" + seconds;
-   }
-   if (seconds <= 0) {
-    stop();
-   }
-   return seconds;
+    var minutes = Math.floor(t / 60);
+    var seconds = t - minutes * 60;
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    return seconds;
   }
- 
+
   function startStage() {
-   var ID = "startStage";
-   report(ID);
-   $("#stageDisplay")
-    .children()
-    .remove();
-   $("#stageDisplay").append("<h1> Click me to start </h1>");
-   $("#stageDisplay h1").attr("id", "clickMe");
-   $("#clickMe").on("click", function() {
+    var ID = "startStage";
+    report(ID);
     $("#stageDisplay")
-     .children()
-     .remove();
-     start();
-    triviaGame();
-   });
+      .children()
+      .remove();
+    // delay function so sound plays is delayed with the same delay of starting banner.
+    setTimeout(function() {
+      startSound.play();
+    }, 1000);
+    $("#display").text("Time remaining: " + time + " seconds");
+    $("#stageDisplay").append("<h1> Click me to start </h1>");
+    $("#stageDisplay h1").attr("id", "clickMe");
+
+    $("#clickMe").on("click", function() {
+      $("#stageDisplay").empty();
+      start();
+
+      triviaGame();
+    });
   }
- 
+
   function setStage(current) {
-   var ID = "setStage";
-   report(ID);
-   $("#stageDisplay").children().remove();
+    var ID = "setStage";
+    report(ID);
+    $("#stageDisplay")
+      .children()
+      .remove();
     var newDiv = $("<div>");
     var quest1 = $("<h1>");
     quest1.attr("id", "question");
@@ -245,11 +253,23 @@ $(document).ready(function() {
     ans4.text(current[countNum].answer4);
     newDiv.append(ans4);
     $("#stageDisplay").html(newDiv);
-}
- 
- 
-  
-  // 
+  }
+
+  function endStage() {
+    $("#stageDisplay").empty();
+    var hOne = $("<h1>");
+    hOne.text("Correct: " + right);
+    $("#stageDisplay").append(hOne);
+    var hTwo = $("<h1>");
+    hTwo.text("Wrong: " + wrong);
+    $("#stageDisplay").append(hTwo);
+    var hThree = $("<h1>");
+    var unAnswered = triviaArray.length - wrong - right;
+    hThree.text("Unanswered: " + unAnswered);
+    $("#stageDisplay").append(hThree);
+  }
+
+  //
   // =============================================================================
   // // ==========================================================================
   // // =======================================================================
@@ -257,30 +277,40 @@ $(document).ready(function() {
   // =======================================================================
   // ==========================================================================
   // =============================================================================
- 
+
   function triviaGame() {
-   var ID = "triviaGame";
-   var pick = "";
-  report(ID);
- 
+    var ID = "triviaGame";
+    var pick = "";
+    report(ID);
+    if (clockRunning) {
       currentArray.push(triviaArray[countNum]);
       setStage(currentArray);
       $("h3").on("click", function() {
         console.log("clicked on", this.id);
-            if ((this.id == currentArray[countNum].correct)) {
-            rightSound.play();
-            console.log("correct pick");
-            } else {
-              wrongSound.play();
-            }
-            countNum++
-            triviaGame()
+        if (this.id == currentArray[countNum].correct) {
+          rightSound.play();
+          right++;
+          console.log(right, " correctly picked");
+        } else {
+          wrongSound.play();
+          wrong++;
+          console.log(wrong, "wrongly picked");
+        }
+        countNum++;
+        console.log(countNum);
+        if (countNum < triviaArray.length) {
+          triviaGame();
+        } else {
+          stop();
+          endStage();
+        }
       });
-}
- 
-  function Game() {
-   startStage();
+    }
   }
- 
+
+  function Game() {
+    startStage();
+  }
+
   Game();
- });
+});
